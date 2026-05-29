29fc4392b97c2e80d58c79626cdbb27aad523122
helloboizzz26 <tri.26102005@gmail.com>
2026-05-29 21:02:17 +0700

    Lab02

--- Lab_02/ex01/cipher/caesar/caesar_cipher.py
+++ Lab_02/ex01/cipher/caesar/caesar_cipher.py
@@ -1,32 +1,27 @@
-from cipher.caesar import ALPHABET
+from .alphabet import ALPHABET
 
 
 class CaesarCipher:
     def __init__(self):
         self.alphabet = ALPHABET
 
+    def _shift_char(self, char: str, key: int) -> str:
+        upper_char = char.upper()
+        if upper_char not in self.alphabet:
+            return char
+
+        letter_index = self.alphabet.index(upper_char)
+        output_index = (letter_index + key) % len(self.alphabet)
+        output_letter = self.alphabet[output_index]
+        return output_letter if char.isupper() else output_letter.lower()
+
     def encrypt_text(self, text: str, key: int) -> str:
-        alphabet_len = len(self.alphabet)
-        text = text.upper()
         encrypted_text = []
 
         for letter in text:
-            letter_index = self.alphabet.index(letter)
-            output_index = (letter_index + key) % alphabet_len
-            output_letter = self.alphabet[output_index]
-            encrypted_text.append(output_letter)
+            encrypted_text.append(self._shift_char(letter, key))
 
         return "".join(encrypted_text)
 
     def decrypt_text(self, text: str, key: int) -> str:
-        alphabet_len = len(self.alphabet)
-        text = text.upper()
-        decrypted_text = []
-
-        for letter in text:
-            letter_index = self.alphabet.index(letter)
-            output_index = (letter_index - key) % alphabet_len
-            output_letter = self.alphabet[output_index]
-            decrypted_text.append(output_letter)
-
-        return "".join(decrypted_text)
+        return self.encrypt_text(text, -key)
